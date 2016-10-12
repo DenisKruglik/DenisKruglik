893d4f29e0ab11f03f5edccb385c80c6ca5fa892
DenisKruglik <d.kruglik11011@gmail.com>
2016-10-12 22:58:58 +0300

    Add files via upload

---
+++ Lesson3/script.js
@@ -0,0 +1,52 @@
+// var a=new Array(100);
+// var f=function(x){
+// 	x=Math.random();
+// 	console.log(x);
+// }
+// a.forEach(f,true);
+
+// var b,a=new Array(10);
+// for (var i = 0; i < a.length; i++) {
+// 	a[i]=Math.random();
+// 	console.log(a[i]);
+// }
+// var t=function(e){
+// 	return e*2;
+// }
+// b=a.map(t);
+// b.forEach(function(w){console.log(w)});
+
+// var c=new Array(10);
+// for (var i = 0; i < c.length; i++) {
+// 	c[i]=function(){alert(i)};
+// };
+// c[1]();
+
+// var c=new Array(10);
+// for (var i = 0; i < c.length; i++) {
+// 	c[i]=(function(x){
+// 		return function(){
+// 			alert(x);
+// 		}
+// 	})(i);
+// }
+// c[2]();
+
+// var x=function(){
+// 	var a=0;
+// 	for (var i = 0; i < arguments.length; i++) {
+// 		a=a+arguments[i];
+// 	}
+// 	return a;
+// }
+// console.log(x(prompt()*1,prompt()*1,prompt()*1));
+
+// fc(prompt()).sayName();
+// function fc(name){
+// 	return {
+// 	name:name,
+// 	age: 24,
+// 	sayName: function(){alert(this.name);}
+// }
+// }
+
---
+++ Lesson4/script.js
@@ -0,0 +1,14 @@
+// var f=0;
+// var i=setInterval(function(){
+// 	window.scrollBy(0,5);
+// 	f++;
+// 	if(f==1600) clearInterval(i);
+// },5);
+
+var table="<table border='1'>\
+	<tr>\
+		<td>Поле</td>\
+		<td>Значение</td>\
+	</tr>\
+</table>";
+document.write(table);
